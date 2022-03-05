377d5ff8e0e630336847560c703e50daa68ec37e
lin2772 <46533075+lin2772@users.noreply.github.com>
2022-03-05 16:41:31 -0500

    Add data import example

--- class.py
+++ class.py
@@ -1,2 +1,22 @@
 # Databricks notebook source
 print("Hello")
+
+# COMMAND ----------
+
+# File location and type
+file_location = "/FileStore/tables/nba.csv/nba.csv"
+file_type = "csv"
+
+# CSV options
+infer_schema = "false"
+first_row_is_header = "false"
+delimiter = ","
+
+# The applied options are for CSV files. For other file types, these will be ignored.
+df = spark.read.format(file_type) \
+  .option("inferSchema", infer_schema) \
+  .option("header", first_row_is_header) \
+  .option("sep", delimiter) \
+  .load(file_location)
+
+display(df)
